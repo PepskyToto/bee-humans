aca23da454f141710c11a77548f0988bac92600e
VERNET Jean-Francois <jfvernet@icloud.com>
2023-12-12 16:04:23 +0100

    Map fonctionnelle, reste à personnaliser et adapter pour telephone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :pollem, :average_rating, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :pollem, :average_rating, :address, :latitude, :longitude])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :pollem, :average_rating, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :pollem, :average_rating, :address, :latitude, :longitude])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :reviews, dependent: :destroy
   has_many :requests,  dependent: :destroy
   has_many :bookmarks, dependent: :destroy
