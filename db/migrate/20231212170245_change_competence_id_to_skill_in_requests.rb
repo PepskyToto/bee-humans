5836db09505d49b27004979ef58e98414b5d61aa
edouard dellamonica edouarrddd@gmail.com <edouarrddd@gmail.com>
2023-12-13 11:02:54 +0100

    chatroom 3

--- db/migrate/20231212170245_change_competence_id_to_skill_in_requests.rb
+++ db/migrate/20231212170245_change_competence_id_to_skill_in_requests.rb
@@ -1,4 +1,7 @@
-class ChangeCompetenceIdToSkillInRequests < ActiveRecord::Migration[7.1]
+class ChangeCompetenceIdToSkillIdInRequests < ActiveRecord::Migration[7.1]
   def change
+    if column_exists?(:requests, :competence_id)
+      rename_column :requests, :competence_id, :skill_id
+    end
   end
 end
