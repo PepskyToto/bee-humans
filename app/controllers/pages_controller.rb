0538502d1746052366ced22e312f4c62f1867a4b
edouard dellamonica edouarrddd@gmail.com <edouarrddd@gmail.com>
2023-12-18 17:54:36 +0100

    user-around

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @users = User.all
-    # The `geocoded` scope filters only flats with coordinates
-    @markers = @users.geocoded.map do |user|
+    @nearby_users = User.near([current_user.latitude, current_user.longitude], 10) # 10 kilomètres de distance (ajustez selon vos besoins)
+    @markers = @nearby_users.map do |user|
       {
         lat: user.latitude,
         lng: user.longitude,
-        info_window_html: render_to_string(partial: "user_info", locals: {user: user}),
+        info_window_html: render_to_string(partial: "user_info", locals: { user: user }),
         marker_html: render_to_string(partial: "marker")
       }
     end
