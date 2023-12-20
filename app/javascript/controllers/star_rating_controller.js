94a4ac6d159bad1127e6c0a900482d02503e3b49
Aurore Bouzac <auroresb@hotmail.fr>
2023-12-20 14:34:51 +0100

    front des pop up et autres conneries

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -3,6 +3,17 @@ import StarRating from "star-rating.js"
 
 export default class extends Controller {
   connect() {
-    new StarRating(this.element)
+    new StarRating(this.element, {
+      classNames: {
+          active: 'gl-active',
+          base: 'gl-star-rating',
+          selected: 'gl-selected',
+      },
+      clearable: true,
+      maxStars: 10,
+      prebuilt: false,
+      stars: null,
+      tooltip: false,
+  })
   }
 }
